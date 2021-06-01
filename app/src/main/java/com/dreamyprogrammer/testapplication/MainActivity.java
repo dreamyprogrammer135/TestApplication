a38e542f324aa1aaf4083c6ff12a918365061f9d
Litvinov Ilya <dreameprogrammer82@gmail.com>
2021-06-01 18:16:40 +0300

    master

--- app/src/main/java/com/dreamyprogrammer/testapplication/MainActivity.java
+++ app/src/main/java/com/dreamyprogrammer/testapplication/MainActivity.java
@@ -1,14 +1,24 @@
 package com.dreamyprogrammer.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button;
+    public EditText editText;
+    public static final String CALC_DATA = "calcData";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
